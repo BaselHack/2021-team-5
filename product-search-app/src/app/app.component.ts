efc4c84b797bac8315a468b6529d8ad8c5a58a8c
Tobias.Beck <tobias.beck.89@gmail.com>
2021-09-11 21:06:19 +0200

    post image to API

--- product-search-app/src/app/app.component.ts
+++ product-search-app/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -12,12 +15,15 @@ export class AppComponent {
 
 
   fileName = '';
+  private url = '/api/v1/product-search';
 
   constructor(private http: HttpClient) {}
 
   onFileSelected(event:any) {
+    console.log('on file selected');
+    const file:File = event.target.files[0];
+    console.log(file);
 
-      const file:File = event.target.files[0];
 
       if (file) {
 
@@ -27,11 +33,25 @@ export class AppComponent {
 
           formData.append("thumbnail", file);
 
-          const upload$ = this.http.post("/api/v1/product-search", formData);
-upload$.pipe().toPromise
+          const upload$ = this.http.post(this.url, formData);
+upload$.pipe(catchError(this.handleError(this.url, [])));
           upload$.subscribe();
       }
   }
 
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      alert(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+
+  }
 
 }
