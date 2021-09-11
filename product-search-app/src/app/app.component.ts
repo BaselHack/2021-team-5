d8547ba145980287be59f3243bab0bdf9eb44de4
Tobias.Beck <tobias.beck.89@gmail.com>
2021-09-11 20:03:36 +0200

    file upload

--- product-search-app/src/app/app.component.ts
+++ product-search-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'product-search-app';
+
+
+
+  fileName = '';
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event:any) {
+
+      const file:File = event.target.files[0];
+
+      if (file) {
+
+          this.fileName = file.name;
+
+          const formData = new FormData();
+
+          formData.append("thumbnail", file);
+
+          const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+          upload$.subscribe();
+      }
+  }
+
+
 }
